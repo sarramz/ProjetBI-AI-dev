456433008475a29b317b6bef7b3bb83026d2d189
sarra <sarramzali12@gmail.com>
2025-02-21 09:53:20 +0100

    first commit

---
+++ src/component/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header( props ) {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+export default Header
---
+++ src/screens/login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Login.css'
+import Header from '../../component/header/Header'
+function Login() {
+
+
+
+return (
+
+    <div>
+        <Header name='leeeeeeeee'/>
+    </div>
+  )
+}
+
+export default Login
